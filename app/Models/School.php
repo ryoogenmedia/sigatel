51cf33394a0c8a0931971d0803056d4f07f8cc86
M-BintangR <muhbintang650@gmail.com>
2025-03-14 06:08:23 +0800

    add: school model & table schema

---
+++ app/Models/School.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class School extends Model
+{
+    use HasFactory;
+
+    protected $table = 'schools';
+
+    protected $fillable = [
+        'name_school',
+        'logo',
+        'address',
+        'email',
+        'telp',
+        'phone',
+        'accreditation',
+    ];
+}
