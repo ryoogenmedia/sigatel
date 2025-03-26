c71510807553d1fd41a9ab6fff1065cff98996a4
M-BintangR <muhbintang650@gmail.com>
2025-03-27 02:36:39 +0800

    fix: modal

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,7 +1,7 @@
-<div class="{{ $show ? 'd-show' : 'd-none' }}">
+<div style="z-index: 99999" class="{{ $show ? 'd-show' : 'd-none' }}">
     <div class="modal-backdrop fade show"></div>
 
-    <div class="modal show" style="padding-right: 17px; display: block;">
+    <div class="modal show" style="display: block;">
         <div class="modal-dialog modal-{{ isset($size) ? $size : 'sm' }} modal-dialog-centered" role="document">
             <div class="modal-content">
                 {{ $slot }}
