ac57dc2460f8af0007ab085b2b6887f8ea78b42a
M-BintangR <muhbintang650@gmail.com>
2025-03-28 01:15:39 +0800

    title application

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Aplikasi School | @yield('title')</title>
+    <title>Smart Piket SMPN 25 Makassar | @yield('title')</title>
 
     <link href="{{ asset('dist/css/tabler.min.css') }}" rel="stylesheet" />
     <link href="{{ asset('dist/css/tabler-flags.min.css') }}" rel="stylesheet" />
