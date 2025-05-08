03bfb320ed3646fbbcda00641b5215d8a85d0be2
M-BintangR <muhbintang650@gmail.com>
2025-05-08 11:35:36 +0800

    [MOBILE] fix: student parent data seeder

--- database/seeders/StudentParentTableSeeder.php
+++ database/seeders/StudentParentTableSeeder.php
@@ -22,8 +22,38 @@ class StudentParentTableSeeder extends Seeder
         $studentIds = Student::pluck('id')->toArray();
         $startDate = Carbon::now()->subYears(5)->startOfYear();
         $endDate = Carbon::now();
-
         $currentDate = $startDate->copy();
+
+        $parents = [
+            [
+                'username'          => 'Pak Fery',
+                'email'             => 'feryparent@gmail.com',
+                'email_verified_at' => now(),
+                'password'          => Hash::make('feryparent123'),
+                'roles'             => 'parent',
+                'name'              => 'Pak Fery',
+                'phone_number'      => '08939834543',
+                'guardian_status'   => $faker->randomElement(['anak angkat','anak kandung']),
+            ]
+        ];
+
+        foreach($parents as $parent){
+            $user = User::create([
+                'username'          => $parent['username'],
+                'email'             => $parent['email'],
+                'email_verified_at' => $parent['email_verified_at'],
+                'password'          => $parent['password'],
+                'roles'             => $parent['roles'],
+            ]);
+
+            StudentParent::create([
+                'user_id'         => $user->id,
+                'student_id'      => $faker->randomElement($studentIds),
+                'name'            => $parent['name'],
+                'phone_number'    => $parent['phone_number'],
+                'guardian_status' => $parent['guardian_status'],
+            ]);
+        }
 
         while ($currentDate <= $endDate) {
             $user = User::create([
